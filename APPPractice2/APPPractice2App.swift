abb4f300c81f708910ca074c44f6e8c4bc751ade
Sarvesh Agrawal <proacc2022@gmail.com>
2022-10-09 14:14:17 -0700

    Initial Commit

---
+++ APPPractice2/APPPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  APPPractice2App.swift
+//  APPPractice2
+//
+//  Created by Sarvesh Agrawal on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct APPPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
